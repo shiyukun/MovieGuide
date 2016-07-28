5cb2a7c0737cf4a69159146a3e6fa6ad396e0a81
shiyukun <yukun.shi@gtdollar.com>
2016-07-28 21:08:11 +0800

    add key

--- app/src/main/java/com/esoxjem/movieguide/constants/Api.java
+++ app/src/main/java/com/esoxjem/movieguide/constants/Api.java
@@ -5,7 +5,7 @@ package com.esoxjem.movieguide.constants;
  */
 public class Api
 {
-    public static final String API_KEY = "";
+    public static final String API_KEY = "2cbb2830fc20680b1eec38b27495c616";
     public static final String GET_POPULAR_MOVIES = "http://api.themoviedb.org/3/discover/movie?language=en&sort_by=popularity.desc&api_key=" + API_KEY;
     public static final String GET_HIGHEST_RATED_MOVIES = "http://api.themoviedb.org/3/discover/movie?vote_count.gte=500&language=en&sort_by=vote_average.desc&api_key=" + API_KEY;
     public static final String GET_TRAILERS = "http://api.themoviedb.org/3/movie/%s/videos?api_key=" + API_KEY;
